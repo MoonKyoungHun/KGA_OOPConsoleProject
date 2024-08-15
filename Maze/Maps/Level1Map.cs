f6be95c8b34bdb89b0874309b6d7c5f490cd3466
MoonKyoungHun <lsf8355@gmail.com>
2024-08-15 14:07:17 +0900

    Level1Map 수정
    수정 및 기능 추가

--- Maze/Maps/Level1Map.cs
+++ Maze/Maps/Level1Map.cs
@@ -5,10 +5,8 @@
         Funtion funtion = new Funtion();
         Player player = new Player();
 
-
         public int x;
         public int y;
-
 
         private bool[,] map;
         public ConsoleKey inputKey;
@@ -17,20 +15,12 @@
         {
             player.pos.x = 1;
             player.pos.y = 1;
-            player.map = map;
-            funtion.goal.x = 0;
-            funtion.goal.y = 0;
         }
 
+        
         public override void Enter()
         {
-            // Map 생성
             Console.CursorVisible = false;
-
-            Console.Clear();
-            Console.WriteLine("=============Level1=============");
-            Console.WriteLine();
-            Console.WriteLine();
 
             map = new bool[,]
             {
@@ -40,7 +30,7 @@
                 { false,  true, false, false, false, false,  true, false, false, false,  true, false},
                 { false,  true, false, false, false, false,  true, false, false, false,  true, false},
                 { false,  true,  true,  true, true,   true,  true,  true,  true, false,  true, false},
-                { false,  true, false, false, false,  true,  true,  true,  true, true,  true, false},
+                { false,  true, false, false, false,  true,  true,  true,  true, false,  true, false},
                 { false,  true, false,  true, false,  true,  true, false, false, false, false, false},
                 { false,  true, false,  true,  true,  true,  true, false, false, false, false, false},
                 { false,  true, false,  true,  true,  true,  true,  true,  true,  true, false, false},
@@ -52,7 +42,7 @@
         public override void Render()
         {
             Console.Clear();
-
+            player.ShowInfo(1);
             PrintMap();
             funtion.PrintGoal(8, 1);
             funtion.PrintWall(7, 1);
@@ -61,8 +51,6 @@
             player.PrintPlayer(player.pos.x, player.pos.y);
             funtion.PrintTemp(10, 10);
             funtion.PrintTemp1(10, 6);
-            funtion.PrintBlink();
-
         }
 
         public override void Input()
@@ -72,12 +60,15 @@
 
         public void PrintMap()
         {
-
             for (int y = 0; y < map.GetLength(0); y++)
             {
                 for (int x = 0; x < map.GetLength(1); x++)
                 {
-                    if (map[y, x])
+                    if (funtion.wall.x == x && funtion.wall.y == y)
+                    {
+                        Console.Write(" ");
+                    }
+                    else if (map[y, x])
                     {
                         Console.Write(" ");
                     }
@@ -88,32 +79,43 @@
                 }
                 Console.WriteLine();
             }
+            
         }
 
         public override void Update()
         {
             player.Move(map);
+            PrintBlink();
             CheckGameClear();
-            game.End();
         }
 
         public override void Exit()
         {
-            CheckGameClear();
         }
 
-       
+        public void PrintBlink()
+        {
+
+            if (player.pos.x == funtion.temp.x && player.pos.y == funtion.temp.y)
+            {
+                player.pos.x = funtion.temp1.x;
+                player.pos.y = funtion.temp1.y;
+            }
+            else if (player.pos.x == funtion.temp1.x && player.pos.y == funtion.temp1.y)
+            {
+                player.pos.x = funtion.temp.x;
+                player.pos.y = funtion.temp.y;
+            }
+        }
+
         public void CheckGameClear()
         {
 
             if (player.pos.x == funtion.goal.x && player.pos.y == funtion.goal.y)
             {
                 Console.Clear();
-                //game.changemap(maptype.level2);
-                
+                game.ChangeMap(MapType.Level2);
             }
-            
-
         }
 
     }
