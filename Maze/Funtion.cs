a378b63273a6a30a94021a831a6825bc2d684265
MoonKyoungHun <lsf8355@gmail.com>
2024-08-15 14:06:45 +0900

    기능 삭제(부분)
    기능 삭제(부분)

--- Maze/Funtion.cs
+++ Maze/Funtion.cs
@@ -2,14 +2,13 @@
 {
     public class Funtion
     {
-        Player player = new Player();
-
         public Point temp;
         public Point temp1;
         public Point goal;
         public Point fake;
         public Point wall;
-        
+        public Point trap;
+
 
         public struct Point()
         {
@@ -23,34 +22,21 @@
             temp.y = y;
 
             Console.SetCursorPosition(temp.x, temp.y);
-            Console.WriteLine("#");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("#");
+            Console.ResetColor();
         }
 
         public void PrintTemp1(int x, int y)
         {
             temp1.x = x;
             temp1.y = y;
+
             Console.SetCursorPosition(temp1.x, temp1.y);
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("#");
+            Console.Write("#");
             Console.ResetColor();
         }
-
-        public void PrintBlink()
-        {
-            
-            if(player.pos.x == temp.x && player.pos.y == temp.y)
-            {
-                player.pos.x = temp1.x;
-                player.pos.y = temp1.y;
-            }
-            else if(player.pos.x == temp1.x && player.pos.y == temp1.y)
-            {
-                player.pos.x = temp.x;
-                player.pos.y = temp.y;
-            }
-        }
-
 
         public void PrintGoal(int x, int y)
         {
@@ -68,9 +54,9 @@
             fake.x = x;
             fake.y = y;
 
-            Console.ForegroundColor = ConsoleColor.Magenta;
             Console.SetCursorPosition(fake.x, fake.y);
-            Console.WriteLine("#");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("#");
             Console.ResetColor();
         }
 
@@ -80,7 +66,16 @@
             wall.y = y;
 
             Console.SetCursorPosition(wall.x, wall.y);
-            Console.WriteLine(" ");
+        }
+
+        public void PrintTrap(int x, int y)
+        {
+            trap.x = x;
+            trap.y = y;
+            Console.SetCursorPosition(trap.x, trap.y);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("#");
+            Console.ResetColor();
         }
     }
 }
